220a178777fe51c2bfb9d8e31084008476732d1e
M1lo97 <61304221+M1lo97@users.noreply.github.com>
2020-05-19 14:27:50 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://secret-earth-46685.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://paty-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
